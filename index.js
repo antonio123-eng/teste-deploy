4e56dea081912050a0e2d83b7bfe0308e02df2ff
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 10:35:08 -0300

    457812

--- index.js
+++ index.js
@@ -34,7 +34,21 @@ require('./config/db.js')
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 // cors
-app.use(cors({credentials: true, origin: "https://teste-deploy-frontend.herokuapp.com"}))
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*")
+   res.header(
+     "Access-Control-Allow-Headers",
+     "Origin, X-Requested, Content-Type, Accept Authorization"
+   )
+   if (req.method === "OPTIONS") {
+     res.header(
+       "Access-Control-Allow-Methods",
+       "POST, PUT, PATCH, GET, DELETE"
+     )
+     return res.status(200).json({})
+   }
+   next()
+ })
 
 
 app.listen(port, () => {
