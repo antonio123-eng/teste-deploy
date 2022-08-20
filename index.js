9733ef663dd31e86f6d48295c36ae4264b560dae
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 14:00:35 -0300

    tyu

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 // cors
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "https://teste-deploy-frontend.herokuapp.com/")
+   res.header("Access-Control-Allow-Origin", "*")
    res.header(
      "Access-Control-Allow-Headers",
      "Origin, X-Requested, Content-Type, Accept Authorization"
