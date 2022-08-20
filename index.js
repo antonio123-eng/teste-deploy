a3767c814a6e1ff86009039add3e7c484a772e66
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 13:57:15 -0300

    r

--- index.js
+++ index.js
@@ -34,7 +34,22 @@ require('./config/db.js')
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 // cors
-app.use(cors({credentials: true,  origin: "https://teste-deploy-frontend.herokuapp.com/", methods: "GET, POST, OPTIONS, DELETE, PUT, PATCH", exposedHeaders: "X-My-Custom-Header, X-Another-Custom-Header", allowedHeaders: "X-PINGOTHER, Content-Type"}))
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "https://teste-deploy-frontend.herokuapp.com/")
+   res.header(
+     "Access-Control-Allow-Headers",
+     "Origin, X-Requested, Content-Type, Accept Authorization"
+   )
+   if (req.method === "OPTIONS") {
+     res.header(
+       "Access-Control-Allow-Methods",
+       "POST, PUT, PATCH, GET, DELETE"
+     )
+     return res.status(200).json({})
+   }
+   next()
+ })
+ //Cors
 
 app.listen(port, () => {
    console.log(`Servidor rodando na porta ${port}`)
