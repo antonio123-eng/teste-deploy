769f1f9f37bcef52ea356c63d5c6363dd305703c
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 13:07:02 -0300

    t

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ require('./config/db.js')
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 // cors
-
+app.use(cors())
 
 app.listen(port, () => {
    console.log(`Servidor rodando na porta ${port}`)
